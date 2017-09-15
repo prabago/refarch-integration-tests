445218d9e3e4feecb53f7f0593dc643bec608504
Jerome Boyer <boyerje@us.ibm.com>
2017-09-14 22:31:31 -0700

    Change some simple test sh script

--- testLDAPAccess.sh
+++ testLDAPAccess.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ldapsearch -x -LLL -h 172.16.50.9  -b "dc=csplab,dc=local" 
+ldapsearch -x -LLL -h 172.16.50.9  -b "dc=csplab,dc=local"
+#ldapsearch -x -LLL -h 172.17.0.1  -b "dc=csplab,dc=local"
