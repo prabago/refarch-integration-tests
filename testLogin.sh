6ad8b01ce3b7d48136988998a772498c71360c3a
Jerome Boyer <boyerje@us.ibm.com>
2017-10-24 10:02:04 -0700

    better login test

--- testLogin.sh
+++ testLogin.sh
@@ -1,3 +1,3 @@
-#curl -k -v -H 'X-IBM-Client-Id:5d2a6edb-793d-4193-b9b0-0a087ea6c123' -H 'Content-Type: application/x-www-form-urlencoded' "https://cap-sg-prd-5.integration.ibmcloud.com:16582/csplab/sb/sample-inventory-api/login?username=case@usibm&password=case01"
-curl -k -v -H 'X-IBM-Client-Id:69b5d2ac-95d5-4aab-9884-4350759e72f3' -H 'Content-Type: application/x-www-form-urlencoded' "https://172.16.50.8/csplab/sb/iib-inventory-api/login?username=case@usibm&password=case01"
+curl -k -v -H 'X-IBM-Client-Id:5d2a6edb-793d-4193-b9b0-0a087ea6c123' -H 'Content-Type: application/x-www-form-urlencoded' "https://cap-sg-prd-5.integration.ibmcloud.com:16582/csplab/sb/sample-inventory-api/login?username=case@usibm&password=case01"
+#curl -k -v -H 'X-IBM-Client-Id:69b5d2ac-95d5-4aab-9884-4350759e72f3' -H 'Content-Type: application/x-www-form-urlencoded' "https://172.16.50.8/csplab/sb/iib-inventory-api/login?username=case@usibm&password=case01"
 
